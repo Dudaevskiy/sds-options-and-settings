52097df393a6dc60f9ea96afa4bac9eace033f96
Serhii Dudchenko <sergeydydchenko@gmail.com>
2020-08-03 17:28:30 +0300

    DEV Google Tag

--- _WORKER.php
+++ _WORKER.php
@@ -1196,7 +1196,7 @@ Redux::set_section( $opt_name__redux_sds_options_and_settings, $section );
  */
 //$SDStudio_CSS_table_styles = $Hot_Keys = $MarkdownParser->parse( file_get_contents(dirname(__FILE__) . '/_markdown/CSS_table_styles.md') );
 $section = [
-    'title' => __( 'Google Adsense', 'arrows_pages_sds-options-and-settings' ),
+    'title' => __( 'DEV - Google Adsense', 'arrows_pages_sds-options-and-settings' ),
 //    'title' => __( 'Переадрисация при входе и выходе ', 'login_redirects-page-posts-sds-options-and-settings' ),
     'id'    => 'google_adsense_sds-options-and-settings',
     'subsection' => false,
@@ -1236,9 +1236,51 @@ Redux::set_section( $opt_name__redux_sds_options_and_settings, $section );
 
 
 /**
- * Google Adsense
+ * Google Менеджер тегов (Google Tag Manager)
  */
-//$SDStudio_CSS_table_styles = $Hot_Keys = $MarkdownParser->parse( file_get_contents(dirname(__FILE__) . '/_markdown/CSS_table_styles.md') );
+$section = [
+    'title' => __( 'DEV -  Google Менеджер тегов (Google Tag Manager)', 'arrows_pages_sds-options-and-settings' ),
+//    'title' => __( 'Переадрисация при входе и выходе ', 'login_redirects-page-posts-sds-options-and-settings' ),
+    'id'    => 'google_tag_manager_sds-options-and-settings',
+    'subsection' => false,
+    // Иконки брать здесь
+    // http://elusiveicons.com/icons/
+    'icon'  => 'el el-view-mode',
+    'desc'  => 'Активируем опции Google Менеджера тегов (Google Tag Manager)',
+//    'ajax_save' => false,
+    'fields' => [
+        [
+            //Link: https://docs.redux.io/core-fields/switch.html
+            'id'       => 'enable_header_code_google_tag_manager_sds-options-and-settings',
+            'type'     => 'switch',
+            'icon'  => 'el el-chevron-right',
+            'title'    => __('Установить Google Менеджер тегов на сайт?', 'redux-framework-demo'),
+            'subtitle' => "Добавляет в header и body страниц сайта код отслеживания Google Tag Manager",
+//            'desc'  => raw("<pre><meta name=\"yandex-verification\" content=\"00017ddb0314e8c\" /></pre>"),
+//                'required' => array('enable_table_CSS_add_design-sds-options-and-settings', '=', 'true' ),
+            //                                'desc' => '<br><br>',
+            'default'  => false,
+        ],
+        [
+            //Link: https://docs.redux.io/core-fields/switch.html
+            'id'       => 'CODE_enable_google_tag_manager_code_sds-options-and-settings',
+            'type'     => 'text',
+            'icon'  => 'el el-chevron-right',
+            'placeholder'  => 'GTM-XXXXXX',
+            'title'    => __('Вставьте ID отслеживания (GTM-XXXXXX)', 'redux-framework-demo'),
+            'subtitle' => 'Вставьте ID из предоставленного Google Tag Manage кода отслеживания (GTM-XXXXXX)',
+//            'desc'  => __('Активация опции добавляет в интерфейс сайта стралки для пред идущей и следующей записи', 'redux-framework-demo'),
+            'required' => array('enable_header_code_google_tag_manager_sds-options-and-settings', '=', 'true' ),
+            //                                'desc' => '<br><br>',
+            'default'  => false,
+        ],
+    ],
+];
+Redux::set_section( $opt_name__redux_sds_options_and_settings, $section );
+
+/**
+ * Yandex
+ */
 $section = [
     'title' => __( 'Yandex', 'arrows_pages_sds-options-and-settings' ),
 //    'title' => __( 'Переадрисация при входе и выходе ', 'login_redirects-page-posts-sds-options-and-settings' ),
