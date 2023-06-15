346edf845849a5306b7c7d2effa884a2374357f1
Dudaevskiy <sergeydydchenko@gmail.com>
2023-06-16 02:17:59 +0300

    Виправив enable_js_descript_code_edit_sds-options-and-settings (виникала помилка при створенны нового запису) + upd to 2.2.37

--- _SDStudio_code_edit_addons.php
+++ _SDStudio_code_edit_addons.php
@@ -5,12 +5,14 @@ global $enable_js_descript_code_edit_sds_options_and_settings;
 $enable_js_descript_code_edit_sds_options_and_settings = $redux['enable_js_descript_code_edit_sds-options-and-settings'];
 if ($enable_js_descript_code_edit_sds_options_and_settings == 1 ){
 
-    add_filter( 'script_loader_tag', 'sdstudio_add_script_handle', 10, 3 );
-    function sdstudio_add_script_handle( $tag, $handle, $src ) {
-        return str_replace( '<script', sprintf(
-            '<script data-handle="%1$s"',
-            esc_attr( $handle )
-        ), $tag );
+    if (!is_admin()){
+        add_filter( 'script_loader_tag', 'sdstudio_add_script_handle', 10, 3 );
+        function sdstudio_add_script_handle( $tag, $handle, $src ) {
+            return str_replace( '<script', sprintf(
+                '<script data-handle="%1$s"',
+                esc_attr( $handle )
+            ), $tag );
+        }
     }
 
 }
